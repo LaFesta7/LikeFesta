43c10fa379a35a9372e186163be6fa5d1ca0ad5b
jin <132440453+05030522@users.noreply.github.com>
2023-08-18 15:42:57 +0900

    [Complete] 회원가입 로그인 완료 (#5)
    * [Complete] 회원가입 로그인 완료
    
    * [Complete] 회원가입 로그인 완료
    
    * [Complete] 회원가입 로그인 완료

---
+++ src/main/java/com/sparta/lafesta/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.sparta.lafesta.common.exception;
+
+import com.sparta.lafesta.common.dto.ApiResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<ApiResponseDto> handleException(IllegalArgumentException ex) {
+        ApiResponseDto apiException = new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return new ResponseEntity<>(
+
+                apiException,
+
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
