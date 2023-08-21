3e991e49bf30a5d0ab2ca740c9f6f529e05460a2
Hyeon woo Kim <wooin.dev@gmail.com>
2023-08-21 17:39:09 +0900

    [Update] Festival dto 일부 수정
    -시각 정보에 패턴추가
    -예매오픈시각, 행사시작시각, 행사종료시각 순으로 멤버필드 순서 변경

--- src/main/java/com/sparta/lafesta/festival/dto/FestivalResponseDto.java
+++ src/main/java/com/sparta/lafesta/festival/dto/FestivalResponseDto.java
@@ -14,9 +14,9 @@ public class FestivalResponseDto {
     private String title;
     private String location;
     private String content;
+    private LocalDateTime reservationOpenDate;
     private LocalDateTime openDate;
     private LocalDateTime endDate;
-    private LocalDateTime reservationOpenDate;
     private String officialLink;
     private List<ReviewResponseDto> reviews;
 
@@ -25,9 +25,9 @@ public class FestivalResponseDto {
         this.title = festival.getTitle();
         this.location = festival.getLocation();
         this.content = festival.getContent();
+        this.reservationOpenDate = festival.getReservationOpenDate();
         this.openDate = festival.getOpenDate();
         this.endDate = festival.getEndDate();
-        this.reservationOpenDate = festival.getReservationOpenDate();
         this.officialLink = festival.getOfficialLink();
         this.reviews = festival.getReviews().stream().
                 map(ReviewResponseDto::new).collect(Collectors.toList());
