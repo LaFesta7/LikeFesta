34f67a3386ced6b9c47d1e4a70bb048c40fbd46c
Haena <johanna0123@naver.com>
2023-08-22 14:16:12 +0900

    [Update] 예외처리 추가
    1. NotFoundException 지정
    2. UnauthorizedException 지정
    3. GlobalExceptionHandler에 위의 예외처리 추가

---
+++ src/main/java/com/sparta/lafesta/common/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package com.sparta.lafesta.common.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+	public UnauthorizedException(String message) {
+		super(message);
+	}
+}
