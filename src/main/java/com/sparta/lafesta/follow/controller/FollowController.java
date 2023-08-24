b52c9c9c7705aacfcde8d9d7befeee698ddc707a
Haena <johanna0123@naver.com>
2023-08-24 10:42:17 +0900

    [Update] 팔로우 기능 - Swagger 적용

--- src/main/java/com/sparta/lafesta/follow/controller/FollowController.java
+++ src/main/java/com/sparta/lafesta/follow/controller/FollowController.java
@@ -5,6 +5,10 @@ import com.sparta.lafesta.common.security.UserDetailsImpl;
 import com.sparta.lafesta.festival.dto.FestivalResponseDto;
 import com.sparta.lafesta.follow.service.FollowService;
 import com.sparta.lafesta.user.dto.SelectUserResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,51 +20,60 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "팔로우 관련 API", description = "팔로우 관련 API 입니다.")
 public class FollowController {
     private final FollowService followService;
 
-    //유저 팔로우 추가
     @PostMapping("/users/{userId}/follows/{followingUserId}")
-    public ResponseEntity<ApiResponseDto> createFollowingUser(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long followingUserId){
+    @Operation(summary = "유저 팔로우 추가", description = "@PathVariable을 통해 followingUserId를 받아와, 해당 User를 팔로우합니다")
+    public ResponseEntity<ApiResponseDto> createFollowingUser(
+            @Parameter(description = "권한 확인 및 추가 정보를 위해 필요한 User 정보")@AuthenticationPrincipal UserDetailsImpl userDetails,
+            @Parameter(name = "festivalId", description = "리뷰를 생성할 festival의 id", in = ParameterIn.PATH) @PathVariable Long followingUserId){
         return followService.followingUser(userDetails,followingUserId);
     }
 
-    //유저 팔로워 목록 조회 - 나를 팔로우 하는 유저
     @GetMapping("/users/{followedUserId}/followers")
-    public ResponseEntity<List<SelectUserResponseDto>> selectFollowers(@AuthenticationPrincipal UserDetailsImpl userDetails){
+    @Operation(summary = "유저 팔로워 목록 조회 - 나를 팔로우 하는 유저", description = "나를 팔로우 하는 유저를 전체 조회합니다.")
+    public ResponseEntity<List<SelectUserResponseDto>> selectFollowers(
+            @Parameter(description = "권한 확인 및 조회 정보를 위해 필요한 User 정보")@AuthenticationPrincipal UserDetailsImpl userDetails){
         List<SelectUserResponseDto> results = followService.selectFollowers(userDetails);
         return ResponseEntity.ok().body(results);
     }
 
-    //유저 팔로잉 목록 조회 - 내가 팔로우 하는 유저
     @GetMapping("/users/{followingUserId}/followings")
-    public ResponseEntity<List<SelectUserResponseDto>> selectFollowingUsers(@AuthenticationPrincipal UserDetailsImpl userDetails){
+    @Operation(summary = "유저 팔로잉 목록 조회 - 내가 팔로우 하는 유저", description = "내가 팔로우 하는 유저를 전체 조회합니다.")
+    public ResponseEntity<List<SelectUserResponseDto>> selectFollowingUsers(
+            @Parameter(description = "권한 확인 및 조회 정보를 위해 필요한 User 정보")@AuthenticationPrincipal UserDetailsImpl userDetails){
         List<SelectUserResponseDto> results = followService.selectFollowingUsers(userDetails);
         return ResponseEntity.ok().body(results);
     }
 
-    //유저 팔로우 취소
     @DeleteMapping("users/{userId}/follows/{followingUserId}")
-    public ResponseEntity<ApiResponseDto> deleteFollowingUser(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long followingUserId){
+    @Operation(summary = "유저 팔로우 취소", description = "@PathVariable을 통해 followingUserId를 받아와, 해당 User의 팔로우를 취소합니다")
+    public ResponseEntity<ApiResponseDto> deleteFollowingUser(
+            @Parameter(description = "권한 확인 및 취소 정보를 위해 필요한 User 정보")@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long followingUserId){
         return followService.unfollowingUser(userDetails, followingUserId);
     }
 
-    //페스티벌 팔로우
     @PostMapping("/users/{userId}/followed-festivals/{festivalId}")
-    public ResponseEntity<ApiResponseDto> createFollowingFestival(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long festivalId){
+    @Operation(summary = "페스티벌 팔로우", description = "@PathVariable을 통해 festivalId를 받아와, 해당 festival을 팔로우합니다")
+    public ResponseEntity<ApiResponseDto> createFollowingFestival(
+            @Parameter(description = "권한 확인 및 추가 정보를 위해 필요한 User 정보")@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long festivalId){
         return followService.followingFestival(userDetails, festivalId);
     }
 
-    //페스티벌 팔로우 목록 조회
     @GetMapping("/users/{userId}/followed-festivals")
-    public ResponseEntity<List<FestivalResponseDto>> selectFollowingFestivals(@AuthenticationPrincipal UserDetailsImpl userDetails){
+    @Operation(summary = "페스티벌 팔로우 목록 조회", description = "내가 팔로우 하는 페스티벌을 전체 조회합니다.")
+    public ResponseEntity<List<FestivalResponseDto>> selectFollowingFestivals(
+            @Parameter(description = "권한 확인 및 조회 정보를 위해 필요한 User 정보")@AuthenticationPrincipal UserDetailsImpl userDetails){
         List<FestivalResponseDto> results = followService.selectFollowingFestivals(userDetails);
         return ResponseEntity.ok().body(results);
     }
 
-    //페스티벌 팔로우 취소
     @DeleteMapping("/users/{userId}/followed-festivals/{festivalId}")
-    public ResponseEntity<ApiResponseDto> deleteFollowingFestival(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long festivalId){
+    @Operation(summary = "페스티벌 팔로우 취소", description = "@PathVariable을 통해 festivalId를 받아와, 해당 festival 팔로우를 취소합니다")
+    public ResponseEntity<ApiResponseDto> deleteFollowingFestival(
+            @Parameter(description = "권한 확인 및 취소 정보를 위해 필요한 User 정보")@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long festivalId){
         return followService.unfollowingFestival(userDetails, festivalId);
     }
 }
