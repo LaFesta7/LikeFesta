6a1132ba9d3a8b403c801161d522f16ca3ee8e9c
Jung Haein <haeinj1215@gmail.com>
2023-08-25 11:21:06 +0900

    [Update] port 설정 수정

--- src/main/java/com/sparta/lafesta/email/config/MailConfiguration.java
+++ src/main/java/com/sparta/lafesta/email/config/MailConfiguration.java
@@ -1,52 +1,48 @@
 package com.sparta.lafesta.email.config;
 
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
-
 @Configuration
 public class MailConfiguration {
 
-    @Value("${spring.mail.host}")
-    private String mailHost;
+  @Value("${spring.mail.host}")
+  private String mailHost;
 
-    @Value("${spring.mail.port}")
-    private Integer mailPort;
+  @Value("${spring.mail.port}")
+  private Integer mailPort;
 
-    @Value("${spring.mail.username}")
-    private String mailUsername;
+  @Value("${spring.mail.username}")
+  private String mailUsername;
 
-    @Value("${spring.mail.password}")
-    private String mailPassword;
+  @Value("${spring.mail.password}")
+  private String mailPassword;
 
-    @Value("${spring.mail.properties.mail.smtp.auth}")
-    private String mailSmtpAuth;
+  @Value("${spring.mail.properties.mail.smtp.auth}")
+  private String mailSmtpAuth;
 
-    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
-    private String mailStartTls;
+  @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+  private String mailStartTls;
 
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(mailHost);
-        mailSender.setPort(mailPort);
+  @Bean
+  public JavaMailSender getJavaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost(mailHost);
+    mailSender.setPort(mailPort);
 
-        mailSender.setUsername(mailUsername);
-        mailSender.setPassword(mailPassword);
+    mailSender.setUsername(mailUsername);
+    mailSender.setPassword(mailPassword);
 
     Properties props = mailSender.getJavaMailProperties();
     props.put("mail.transport.protocol", "smtp");
     props.put("mail.smtp.auth", mailSmtpAuth);
     props.put("mail.smtp.starttls.enable", mailStartTls);
     props.put("mail.debug", "true");
-    //465 port용 설정
-    props.put("mail.smtp.socketFactory.port", "465");
-    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-        return mailSender;
-    }
+    return mailSender;
+  }
 }
