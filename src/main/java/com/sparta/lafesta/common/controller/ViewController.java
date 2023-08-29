9339c049821a5c4cb0e58aa0451f701d6cee95b3
05030522 <kjinhx96@gmail.com>
2023-08-30 05:09:44 +0900

    [Front] 기본 views 생성

--- src/main/java/com/sparta/lafesta/common/controller/ViewController.java
+++ src/main/java/com/sparta/lafesta/common/controller/ViewController.java
@@ -3,36 +3,48 @@ package com.sparta.lafesta.common.controller;
 import com.sparta.lafesta.social.service.KakaoService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ViewController {
     private final KakaoService kakaoService;
+
+    @Value("${kakaoAppKey}")
+    private String kakaoAppKey;
 
     @GetMapping("/")
     public String home() {
         return "index";
     }
 
-    @GetMapping("/api/users/sign-up")
+    @GetMapping("/users/sign-up")
     public String signUp() {
         return "signup";
     }
 
-    @GetMapping("/api/users/login-page")
-    public String login(Model model) {
-        model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
-        return "login";
+    @GetMapping("/users/signup")
+    public String signUp2() {
+        return "sign-up";
     }
 
-    @GetMapping("/api/users/festivals-map")
+    @GetMapping("/users/festivals-map")
     public String festivalsMap() {
         return "kakaomap";
     }
 
-
-
+    @GetMapping("/users/login-page")
+    public String login(Model model) {
+            String kakaoUrl = kakaoService.getKakaoLogin();
+            model.addAttribute("kakaoUrl", kakaoUrl);
+            model.addAttribute("kakaoAppKey", kakaoAppKey);
+            return "login";
+    }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,29 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 토큰을 확인합니다.
+    // Fetch the user token from local storage
     var token = localStorage.getItem("userToken");
 
-    // 로그인 및 로그아웃 버튼을 찾습니다.
-    var loginButton = document.querySelector(".button[href='login.html']");
-    var logoutButton = document.querySelector(".button[href='']");
+    // Get references to the login and logout elements
+    var loginButton = document.getElementById("loginButton");
+    var logoutForm = document.getElementById("logoutForm");
 
-    // 토큰이 있을 경우 로그인 버튼을 숨기고 로그아웃 버튼을 표시합니다.
+    // If the user is logged in (token exists), hide the login button and show the logout button
     if (token) {
         loginButton.style.display = "none";
-        logoutButton.style.display = "block";
+        logoutForm.style.display = "block";
     }
-    // 토큰이 없을 경우 로그아웃 버튼을 숨기고 로그인 버튼을 표시합니다.
+    // If the user is not logged in (no token), show the login button and hide the logout button
     else {
         loginButton.style.display = "block";
-        logoutButton.style.display = "none";
+        logoutForm.style.display = "none";
     }
 });
-
-// 초기에 두 버튼을 숨기기 위한 스타일을 추가합니다.
-var styleElement = document.createElement("style");
-styleElement.innerHTML = `
-    .button[href='login.html'],
-    .button[href=''] {
-        display: none;
-    }
-`;
-document.head.appendChild(styleElement);
