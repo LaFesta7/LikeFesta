ef2b347107c8741eddc5b1d135498ca5f952084e
Haena <johanna0123@naver.com>
2023-08-30 21:08:00 +0900

    [Update] 코드 정렬

--- src/main/java/com/sparta/lafesta/festival/dto/FestivalRequestDto.java
+++ src/main/java/com/sparta/lafesta/festival/dto/FestivalRequestDto.java
@@ -1,36 +1,38 @@
 package com.sparta.lafesta.festival.dto;
 
 import com.sparta.lafesta.tag.dto.TagRequestDto;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 public class FestivalRequestDto {
 
-  private String title;
-  private String location;
-  private String content;
+    private String title;
+    private String location;
+    private String content;
 
-  //예매 오픈 시각
-  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-  private LocalDateTime reservationOpenDate;
+    //예매 오픈 시각
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime reservationOpenDate;
 
-  // 예매처
-  private String reservationPlace;
+    // 예매처
+    private String reservationPlace;
 
-  //행사 시작 시각
-  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-  private LocalDateTime openDate;
+    //행사 시작 시각
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime openDate;
 
-  //행사 종료 시각
-  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-  private LocalDateTime endDate;
+    //행사 종료 시각
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime endDate;
 
 
-  private String officialLink;
+    private String officialLink;
 
-  private List<TagRequestDto> tagList = new ArrayList<>();
+    private List<TagRequestDto> tagList = new ArrayList<>();
 }
--- src/main/java/com/sparta/lafesta/festival/entity/Festival.java
+++ src/main/java/com/sparta/lafesta/festival/entity/Festival.java
@@ -19,9 +19,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,82 +40,82 @@ import lombok.Setter;
 @Table(name = "festivals")
 public class Festival extends Timestamped {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(name = "title", nullable = false)
-  private String title;
+    @Column(name = "title", nullable = false)
+    private String title;
 
-  @Column(name = "location", nullable = false)
-  private String location;
+    @Column(name = "location", nullable = false)
+    private String location;
 
-  @Column(name = "content", nullable = false)
-  private String content;
+    @Column(name = "content", nullable = false)
+    private String content;
 
-  @Column(name = "open_date", nullable = false)
-  private LocalDateTime openDate;
+    @Column(name = "open_date", nullable = false)
+    private LocalDateTime openDate;
 
-  @Column(name = "end_date", nullable = false)
-  private LocalDateTime endDate;
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
 
-  @Column(name = "reservation_open_date", nullable = false)
-  private LocalDateTime reservationOpenDate;
+    @Column(name = "reservation_open_date", nullable = false)
+    private LocalDateTime reservationOpenDate;
 
-  @Column(name = "reservation_place", nullable = false)
-  private String reservationPlace;
+    @Column(name = "reservation_place", nullable = false)
+    private String reservationPlace;
 
-  @Column(name = "official_link", nullable = false)
-  private String officialLink;
+    @Column(name = "official_link", nullable = false)
+    private String officialLink;
 
-  //연관관계
+    //연관관계
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id", nullable = false)
-  private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
 
-  @OneToMany(mappedBy = "festival", orphanRemoval = true)
-  private List<Review> reviews = new ArrayList<>();
+    @OneToMany(mappedBy = "festival", orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
 
-  @OneToMany(mappedBy = "followedFestival", cascade = CascadeType.PERSIST, orphanRemoval = true)
-  private List<FestivalFollow> festivalFollowers = new ArrayList<>();
+    @OneToMany(mappedBy = "followedFestival", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<FestivalFollow> festivalFollowers = new ArrayList<>();
 
-  @OneToMany(mappedBy = "festival", orphanRemoval = true)
-  private List<FestivalLike> festivalLikes = new ArrayList<>();
+    @OneToMany(mappedBy = "festival", orphanRemoval = true)
+    private List<FestivalLike> festivalLikes = new ArrayList<>();
 
-  @OneToMany(mappedBy = "festival", orphanRemoval = true) //todo 이후 프론트 전달 방식과 관련해서 개선필요해 보임
-  private List<FestivalFileOnS3> festivalFileOnS3s = new ArrayList<>();
+    @OneToMany(mappedBy = "festival", orphanRemoval = true) //todo 이후 프론트 전달 방식과 관련해서 개선필요해 보임
+    private List<FestivalFileOnS3> festivalFileOnS3s = new ArrayList<>();
 
-  @OneToMany(mappedBy = "festival", cascade = CascadeType.PERSIST, orphanRemoval = true)
-  private List<FestivalTag> tags = new ArrayList<>();
+    @OneToMany(mappedBy = "festival", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<FestivalTag> tags = new ArrayList<>();
 
-  ////생성자 - 약속된 형태로만 생성가능하도록 합니다.
+    ////생성자 - 약속된 형태로만 생성가능하도록 합니다.
 
-  public Festival(FestivalRequestDto requestDto, User user) {
+    public Festival(FestivalRequestDto requestDto, User user) {
 
-    this.title = requestDto.getTitle();
-    this.location = requestDto.getLocation();
-    this.content = requestDto.getContent();
-    this.openDate = requestDto.getOpenDate();
-    this.endDate = requestDto.getEndDate();
-    this.reservationOpenDate = requestDto.getReservationOpenDate();
-    this.reservationPlace = requestDto.getReservationPlace();
-    this.officialLink = requestDto.getOfficialLink();
-    this.user = user;
-  }
+        this.title = requestDto.getTitle();
+        this.location = requestDto.getLocation();
+        this.content = requestDto.getContent();
+        this.openDate = requestDto.getOpenDate();
+        this.endDate = requestDto.getEndDate();
+        this.reservationOpenDate = requestDto.getReservationOpenDate();
+        this.reservationPlace = requestDto.getReservationPlace();
+        this.officialLink = requestDto.getOfficialLink();
+        this.user = user;
+    }
 
-  //// 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
+    //// 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
 
-  public void modify(FestivalRequestDto requestDto) {
-    this.title = requestDto.getTitle();
-    this.location = requestDto.getLocation();
-    this.content = requestDto.getContent();
-    this.openDate = requestDto.getOpenDate();
-    this.endDate = requestDto.getEndDate();
-    this.reservationOpenDate = requestDto.getReservationOpenDate();
-    this.reservationPlace = requestDto.getReservationPlace();
-    this.officialLink = requestDto.getOfficialLink();
-  }
+    public void modify(FestivalRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.location = requestDto.getLocation();
+        this.content = requestDto.getContent();
+        this.openDate = requestDto.getOpenDate();
+        this.endDate = requestDto.getEndDate();
+        this.reservationOpenDate = requestDto.getReservationOpenDate();
+        this.reservationPlace = requestDto.getReservationPlace();
+        this.officialLink = requestDto.getOfficialLink();
+    }
 
 }
