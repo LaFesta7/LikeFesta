f807ae79eba8da80e5443c353488948a8008261b
Haena <johanna0123@naver.com>
2023-08-30 21:48:20 +0900

    [Update] 코드 Optimize

--- src/main/java/com/sparta/lafesta/festival/controller/FestivalController.java
+++ src/main/java/com/sparta/lafesta/festival/controller/FestivalController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
