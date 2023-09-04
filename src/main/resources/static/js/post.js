87f92d84c8fab94ea7a5399cc49b15f8471d6e29
05030522 <kjinhx96@gmail.com>
2023-09-04 09:07:35 +0900

    [Front] Ajax, Model

--- src/main/resources/static/js/post.js
+++ src/main/resources/static/js/post.js
@@ -7,18 +7,18 @@ $(document).ready(function () {
             let html = `
             <div class="festival-header">
                 <h1>${data.title}</h1>
-                  <br>
-                  <p> 시작일 </p>
-                  <p>${data.openDate}</p>
-                  <p> 종료일 </p>
-                  <p>${data.endDate}</p>
-                  <br>
-                  <p> 위치 </p>
-                     <p>${data.location}</p>
+                    <br>
+                    <p> 시작일 </p>
+                    <p>${data.openDate}</p>
+                    <p> 종료일 </p>
+                    <p>${data.endDate}</p>
+                    <br>
+                    <p> 위치 </p>
+                    p>${data.location}</p>
             </div>
-                       <div class="festival-content">
-                       <img src="/images/best1.jpg" alt="축제 이미지" class="festival-image">
-                       <p class="festival-description">${data.content}</p>
+                    <div class="festival-content">
+                    <img src="/images/best1.jpg" alt="축제 이미지" class="festival-image">
+                    <p class="festival-description">${data.content}</p>
                 `;
             $('#festival-post').html(html);
         },
