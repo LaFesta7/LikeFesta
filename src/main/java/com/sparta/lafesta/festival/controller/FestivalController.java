edf4920ec3b7932ed04d969c0f85cc1b78ea1557
Haena <johanna0123@naver.com>
2023-09-06 12:16:49 +0900

    [Update] 페스티벌 작성 프론트 추가
    (사진과 함께 작성할 경우 오류 발생한 상태)

--- src/main/java/com/sparta/lafesta/festival/controller/FestivalController.java
+++ src/main/java/com/sparta/lafesta/festival/controller/FestivalController.java
@@ -33,12 +33,13 @@ public class FestivalController {
     @Operation(summary = "페스티벌 등록", description = "페스티벌을 생성합니다. Dto를 통해 정보를 받아와 festival을 생성할 때 해당 정보를 저장합니다.")
     public ResponseEntity<ApiResponseDto> createFestival(
             @Parameter(description = "festival을 생성할 때 필요한 정보") @RequestPart(value = "requestDto") FestivalRequestDto requestDto,
-//            @Parameter(description = "festival을 생성할 때 필요한 정보") @RequestBody FestivalRequestDto requestDto,
             @Parameter(description = "festival 생성시 등록한 첨부 파일") @RequestPart(value = "files", required = false) List<MultipartFile> files,
+//            @Parameter(description = "festival을 생성할 때 필요한 정보") @RequestBody FestivalRequestDto requestDto,
             @Parameter(description = "권한 확인을 위해 필요한 User 정보") @AuthenticationPrincipal UserDetailsImpl userDetails
     ) throws IOException {
 
         FestivalResponseDto result = festivalService.createFestival(requestDto, files, userDetails.getUser());
+//        FestivalResponseDto result = festivalService.createFestival(requestDto, userDetails.getUser());
         return ResponseEntity.ok().body(new ApiResponseDto(HttpStatus.CREATED.value(), result.getTitle()+"를 추가했습니다."));
     }
 
