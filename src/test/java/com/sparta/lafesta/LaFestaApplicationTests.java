e5602aab5b3901a2a6468a1eed58bc7150d7df90
Hyeon woo Kim <wooin.dev@gmail.com>
2023-09-07 09:40:19 +0900

    [fix] 테스트파일 복구

---
+++ src/test/java/com/sparta/lafesta/LaFestaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sparta.lafesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootTest(classes = LaFestaApplication.class)
+@EnableScheduling
+class LaFestaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
