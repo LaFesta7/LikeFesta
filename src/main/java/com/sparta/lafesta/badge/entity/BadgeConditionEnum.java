085a30759f40d0fcafe4af3225ca48b7a918ac54
Haena <johanna0123@naver.com>
2023-09-07 20:49:41 +0900

    [Update] BadgeConditionEnum 주석 수정

--- src/main/java/com/sparta/lafesta/badge/entity/BadgeConditionEnum.java
+++ src/main/java/com/sparta/lafesta/badge/entity/BadgeConditionEnum.java
@@ -1,7 +1,7 @@
 package com.sparta.lafesta.badge.entity;
 
 public enum BadgeConditionEnum {
-    FREQUENCY,  // 페스티벌 참여 빈도수
-    STEADY,  // 리뷰 작성 빈도수
-    TAG;  // 특정 장르 / 종류 빈도수
+    FREQUENCY,  // 페스티벌 참여 빈도수 ex. (기간) N회 참가
+    STEADY,  // 꾸준함 빈도수 ex. N년 연속 참가
+    TAG;  // 특정 장르 / 종류 빈도수 ex. 지역 축제 N회 이상 참가
 }
