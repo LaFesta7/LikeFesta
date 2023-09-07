6a2175fc96863b06dd08c6601dc4355bef4cd4c0
Haena <johanna0123@naver.com>
2023-09-07 20:49:21 +0900

    [Update] 권한이 관리자인 경우에만 메인페이지에서 admin page 이동 경로가 보이도록 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -11,6 +11,14 @@ $(document).ready(function () {
         $('#loginButton').hide();  // 로그인 버튼을 숨깁니다
     }
 
+    // JWT 토큰의 권한 확인
+    const tokenPayload = parseJwtPayload(token); // JWT 토큰을 해석하여 payload를 가져오는 함수로 직접 구현해야 합니다.
+    const role = tokenPayload.auth;
+
+    if (role === 'ADMIN') {
+        $('#adminPageLink').show();
+    }
+
     $.ajax({
         url: '/api/festivals',
         type: 'GET',
@@ -68,6 +76,16 @@ $(document).ready(function () {
         }
     });
 });
+
+function parseJwtPayload(token) {
+    const base64Url = token.split('.')[1]; // JWT의 두 번째 부분이 페이로드입니다.
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // URL 안전한 Base64 문자열로 변환
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join('')); // Base64 디코딩 및 URL 디코딩
+
+    return JSON.parse(jsonPayload); // JSON 문자열을 객체로 파싱
+}
 
 async function moveAdminPage() {
     try {
