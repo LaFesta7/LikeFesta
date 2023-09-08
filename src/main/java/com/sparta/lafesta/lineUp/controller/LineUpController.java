ccc2f8e29ecc9b533b49215d0343097f249eb980
05030522 <kjinhx96@gmail.com>
2023-09-08 12:30:56 +0900

    [feature] Lineup,spotify api 임시

--- src/main/java/com/sparta/lafesta/lineUp/controller/LineUpController.java
+++ src/main/java/com/sparta/lafesta/lineUp/controller/LineUpController.java
@@ -4,13 +4,17 @@ import com.sparta.lafesta.common.security.UserDetailsImpl;
 import com.sparta.lafesta.lineUp.dto.LineUpRequestDto;
 import com.sparta.lafesta.lineUp.dto.LineUpResponseDto;
 import com.sparta.lafesta.lineUp.service.LineUpService;
+import com.sparta.lafesta.user.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,31 +26,37 @@ public class LineUpController {
     @GetMapping("/festivals/{festivalId}/lineups/{lineUpId}")
     @Operation(summary = "페스티벌 라인업 조회", description = "페스티벌 라인업을 조회합니다.")
     public ResponseEntity<LineUpResponseDto> findLineUps(@PathVariable Long festivalId, @PathVariable Long lineUpId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        LineUpResponseDto lineUp = lineUpService.findLineUps(festivalId, lineUpId);
-        return ResponseEntity.ok(lineUp);
+        try {
+            User user = userDetails.getUser();
+            LineUpResponseDto lineUp = lineUpService.findLineUps(festivalId, lineUpId, user);
+            return ResponseEntity.ok(lineUp);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/festivals/{festivalId}/lineups")
     @Operation(summary = "페스티벌 라인업 저장", description = "페스티벌 라인업을 저장합니다.")
     public ResponseEntity<LineUpResponseDto> saveLineUp(@PathVariable Long festivalId, @RequestBody LineUpRequestDto lineUpRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Long userId = userDetails.getUserId();
-        LineUpResponseDto savedLineUp = lineUpService.saveLineUp(festivalId, lineUpRequestDto, userId);
+        User user = userDetails.getUser();
+        LineUpResponseDto savedLineUp = lineUpService.saveLineUp(festivalId, lineUpRequestDto, user);
         return new ResponseEntity<>(savedLineUp, HttpStatus.CREATED);
     }
 
     @PutMapping("/festivals/{festivalId}/lineups/{lineUpId}")
     @Operation(summary = "페스티벌 라인업 수정", description = "페스티벌 라인업을 수정합니다.")
     public ResponseEntity<LineUpResponseDto> updateLineUp(@PathVariable Long festivalId, @PathVariable Long lineUpId, @RequestBody LineUpRequestDto lineUpRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Long userId = userDetails.getUserId();
-        LineUpResponseDto updatedLineUp = lineUpService.updateLineUp(festivalId, lineUpId, lineUpRequestDto, userId);
+        User user = userDetails.getUser();
+        LineUpResponseDto updatedLineUp = lineUpService.updateLineUp(festivalId, lineUpId, lineUpRequestDto, user);
         return new ResponseEntity<>(updatedLineUp, HttpStatus.OK);
     }
 
+    @Transactional
     @DeleteMapping("/festivals/{festivalId}/lineups/{lineUpId}")
     @Operation(summary = "페스티벌 라인업 삭제", description = "페스티벌 라인업을 삭제합니다.")
     public ResponseEntity<Void> deleteLineUp(@PathVariable Long festivalId, @PathVariable Long lineUpId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Long userId = userDetails.getUserId();
-        lineUpService.deleteLineUp(festivalId, lineUpId, userId);
+        User user = userDetails.getUser();
+        lineUpService.deleteLineUp(festivalId, lineUpId, user);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/sparta/lafesta/lineUp/service/LineUpServiceImpl.java
+++ src/main/java/com/sparta/lafesta/lineUp/service/LineUpServiceImpl.java
@@ -1,90 +1,72 @@
-    package com.sparta.lafesta.lineUp.service;
+package com.sparta.lafesta.lineUp.service;
 
-    import com.sparta.lafesta.common.exception.UnauthorizedException;
-    import com.sparta.lafesta.festival.entity.Festival;
-    import com.sparta.lafesta.festival.repository.FestivalRepository;
-    import com.sparta.lafesta.festival.service.FestivalService;
-    import com.sparta.lafesta.lineUp.dto.LineUpRequestDto;
-    import com.sparta.lafesta.lineUp.dto.LineUpResponseDto;
-    import com.sparta.lafesta.lineUp.entity.LineUp;
-    import com.sparta.lafesta.lineUp.repository.LineUpRepository;
-    import com.sparta.lafesta.user.entity.User;
-    import lombok.RequiredArgsConstructor;
-    import org.springframework.stereotype.Service;
-    import org.springframework.transaction.annotation.Transactional;
+import com.sparta.lafesta.festival.entity.Festival;
+import com.sparta.lafesta.festival.repository.FestivalRepository;
+import com.sparta.lafesta.festival.service.FestivalService;
+import com.sparta.lafesta.lineUp.dto.LineUpRequestDto;
+import com.sparta.lafesta.lineUp.dto.LineUpResponseDto;
+import com.sparta.lafesta.lineUp.entity.LineUp;
+import com.sparta.lafesta.lineUp.repository.LineUpRepository;
+import com.sparta.lafesta.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-    @Service
-    @RequiredArgsConstructor
-    public class LineUpServiceImpl implements LineUpService {
+import java.io.IOException;
 
-        private final LineUpRepository lineUpRepository;
-        private final FestivalRepository festivalRepository;
-        private final FestivalService festivalService;
+@Service
+@RequiredArgsConstructor
+public class LineUpServiceImpl implements LineUpService {
 
-        @Override
-        @Transactional
-        public LineUpResponseDto saveLineUp(Long festivalId, LineUpRequestDto requestDto, User user) {
-            // 사용자에 대한 페스티벌을 확인
-            Festival festival = getFestivalForUser(festivalId, user);
-            LineUp savedLineUp = lineUpRepository.save(requestDto.toEntity(festival));
-            return new LineUpResponseDto(savedLineUp);
-        }
+    private final LineUpRepository lineUpRepository;
+    private final FestivalRepository festivalRepository;
+    private final FestivalService festivalService;
 
-        @Override
-        @Transactional
-        public LineUpResponseDto updateLineUp(Long festivalId, Long lineUpId, LineUpRequestDto requestDto, User user) {
-            // 페스티벌 및 사용자 권한 확인
-            getFestivalForUser(festivalId, user);
-
-            return lineUpRepository.findById(lineUpId)
-                    .map(lineUp -> {
-                        lineUp.update(requestDto.getArtistName(), requestDto.getArtistImage(), requestDto.getArtistGenre(), requestDto.getArtistDescription());
-                        return new LineUpResponseDto(lineUp);
-                    })
-                    .orElseThrow(() -> new IllegalArgumentException("해당 ID의 라인업을 찾을 수 없습니다: " + lineUpId));
-        }
-
-        @Override
-        @Transactional
-        public void deleteLineUp(Long festivalId, Long lineUpId, User user) {
-            // 페스티벌 및 사용자 권한 확인
-            getFestivalForUser(festivalId, user);
-            if (!lineUpRepository.existsById(lineUpId)) {
-                throw new IllegalArgumentException("해당 ID의 라인업을 찾을 수 없습니다: " + lineUpId);
-            }
-            lineUpRepository.deleteById(lineUpId);
-        }
-
-        @Override
-        @Transactional(readOnly = true)
-        public LineUpResponseDto findLineUps(Long festivalId, Long lineUpId) {
-            LineUp lineUp = lineUpRepository.findById(lineUpId)
-                    .orElseThrow(() -> new IllegalArgumentException("해당 ID의 라인업을 찾을 수 없습니다: " + lineUpId));
-            return new LineUpResponseDto(lineUp);
-        }
-
-        @Override
-        public LineUpResponseDto saveLineUp(Long festivalId, LineUpRequestDto lineUpRequestDto) {
-            return null;
-        }
-
-        @Override
-        public LineUpResponseDto updateLineUp(Long festivalId, Long lineUpId, LineUpRequestDto lineUpRequestDto) {
-            return null;
-        }
-
-        @Override
-        public void deleteLineUp(Long festivalId, Long lineUpId) {
-
-        }
-
-        private Festival getFestivalForUser(Long festivalId, User user) {
-            Festival festival = festivalRepository.findById(festivalId)
-                    .orElseThrow(() -> new IllegalArgumentException("해당 ID의 페스티벌을 찾을 수 없습니다: " + festivalId));
-
-            if (!festivalService.isFestivalAdmin(festival, user)) {
-                throw new UnauthorizedException("필요한 권한이 없습니다.");
-            }
-            return festival;
-        }
+    @Override
+    @Transactional(readOnly = true)
+    public LineUpResponseDto findLineUps(Long festivalId, Long lineUpId, User user) throws IOException {
+        LineUp lineUp = lineUpRepository.findById(lineUpId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 라인업을 찾을 수 없습니다: " + lineUpId));
+        return new LineUpResponseDto(lineUp);
     }
+
+    @Override
+    @Transactional
+    public LineUpResponseDto saveLineUp(Long festivalId, LineUpRequestDto requestDto, User user) {
+        // 사용자에 대한 페스티벌을 확인
+        Festival festival = getFestivalForUser(festivalId, user);
+        LineUp savedLineUp = lineUpRepository.save(requestDto.toEntity(festival));
+        return new LineUpResponseDto(savedLineUp);
+    }
+
+    @Override
+    @Transactional
+    public LineUpResponseDto updateLineUp(Long festivalId, Long lineUpId, LineUpRequestDto requestDto, User user) {
+        // 페스티벌 및 사용자 권한 확인
+        getFestivalForUser(festivalId, user);
+
+        return lineUpRepository.findById(lineUpId)
+                .map(lineUp -> {
+                    lineUp.update(requestDto.getArtistName(), requestDto.getArtistImage(), requestDto.getArtistGenre(), requestDto.getArtistDescription());
+                    return new LineUpResponseDto(lineUp);
+                })
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 라인업을 찾을 수 없습니다: " + lineUpId));
+    }
+
+    @Override
+    @Transactional
+    public void deleteLineUp(Long festivalId, Long lineUpId, User user) {
+        // 페스티벌 및 사용자 권한 확인
+        getFestivalForUser(festivalId, user);
+        if (!lineUpRepository.existsById(lineUpId)) {
+            throw new IllegalArgumentException("해당 ID의 라인업을 찾을 수 없습니다: " + lineUpId);
+        }
+        lineUpRepository.deleteById(lineUpId);
+    }
+
+    private Festival getFestivalForUser(Long festivalId, User user) {
+        Festival festival = festivalRepository.findById(festivalId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 페스티벌을 찾을 수 없습니다: " + festivalId));
+        return festival;
+    }
+}
